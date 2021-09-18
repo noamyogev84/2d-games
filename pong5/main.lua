5e8ea660054fbd30b9de48eb1ba358247cf47b2b
Yogev, Noam <noam.yogev@intel.com>
2021-09-18 21:40:53 +0300

    moving to oop mode

---
+++ pong5/main.lua
@@ -0,0 +1,102 @@
+-- Library that allows drawing at virtual resolution, very nice for retro awsthetic
+-- https://github.com/Ulydev/push
+push = require 'push'
+Class = require 'class'
+
+require 'Paddle'
+require 'Ball'
+
+WINDOW_WIDTH = 1280
+WINDOW_HEIGHT = 720
+
+VIRTUAL_WIDTH = 432
+VIRTUAL_HEIGHT = 243
+
+PADDLE_SPEED = 250
+
+-- Called by love on game start
+function love.load()
+
+    -- Setting filtering to nearest neighbour to prevent blurriness
+    love.graphics.setDefaultFilter('nearest', 'nearest')
+
+    math.randomseed(os.time())
+    
+    -- Setting font in love
+    smallFont = love.graphics.newFont('font.ttf', 8)
+    love.graphics.setFont(smallFont)
+
+    push:setupScreen(VIRTUAL_WIDTH, VIRTUAL_HEIGHT, WINDOW_WIDTH, WINDOW_HEIGHT, {
+        fullscreen = false,
+        resizable = true,
+        vsync = true
+    })
+
+    player1 = Paddle(10, 30, 5, 20)
+    player2 = Paddle(VIRTUAL_WIDTH - 10,VIRTUAL_HEIGHT - 30, 5, 20)
+
+    ball = Ball(VIRTUAL_WIDTH / 2 - 2, VIRTUAL_HEIGHT / 2 - 2, 4, 4)
+
+    gameState = 'start'
+end
+
+-- Called by love on each frame with dt in seconds since last frame
+function love.update(dt)
+    if love.keyboard.isDown('w') then
+        player1.dy = -PADDLE_SPEED
+    elseif love.keyboard.isDown('s') then
+        player1.dy = PADDLE_SPEED
+    else
+        player1.dy = 0
+    end
+
+    if love.keyboard.isDown('up') then
+        player2.dy = -PADDLE_SPEED
+    elseif love.keyboard.isDown('down') then
+        player2.dy = PADDLE_SPEED
+    else
+        player2.dy = 0
+    end
+
+    if gameState == 'play' then
+        ball:update(dt)
+    end
+
+    player1:update(dt)
+    player2:update(dt)
+end
+
+-- Called by love on game start (access key by string name)
+function love.keypressed(key)
+    if key == 'escape' then
+        love.event.quit()
+    elseif key == 'enter' or key == 'return' then
+        if gameState == 'start' then
+            gameState = 'play'
+        else
+            gameState = 'start'
+            ball:reset()
+        end
+    end
+end
+
+-- Called by love on each frame (after love.update)
+function love.draw()
+    push:apply('start')
+
+    -- Clear canvas with specific color
+    love.graphics.clear(40/255, 45/255, 52/255, 255/255)
+    love.graphics.setFont(smallFont)
+
+    if gameState == 'start' then
+        love.graphics.printf('Hello Start State!', 0, 20, VIRTUAL_WIDTH, 'center')
+    else
+        love.graphics.printf('Hello Play State!', 0, 20, VIRTUAL_WIDTH, 'center')
+    end
+
+    player1:render()
+    player2:render()
+    ball:render()
+
+    push:apply('end')
+end
