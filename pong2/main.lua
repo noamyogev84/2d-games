68468b8c28f07a2f61a3dd5a450964234e8f383d
Yogev, Noam <noam.yogev@intel.com>
2021-09-18 15:16:38 +0300

    adding paddels

--- pong2/main.lua
+++ pong2/main.lua
@@ -11,6 +11,11 @@ VIRTUAL_HEIGHT = 243
 -- Called by love on game start
 function love.load()
     love.graphics.setDefaultFilter('nearest', 'nearest')
+    
+    -- Setting font in love
+    smallFont = love.graphics.newFont('font.ttf', 8)
+    love.graphics.setFont(smallFont)
+
     push:setupScreen(VIRTUAL_WIDTH, VIRTUAL_HEIGHT, WINDOW_WIDTH, WINDOW_HEIGHT, {
         fullscreen = false,
         resizable = false,
@@ -28,12 +33,15 @@ end
 -- Called by love on each frame (after love.update)
 function love.draw()
     push:apply('start')
-    love.graphics.printf(
-        'Yo yo yo starting to learn lua',
-        0,
-        VIRTUAL_HEIGHT / 2 - 6,
-        VIRTUAL_WIDTH,
-        'center'
-    )
+
+    -- Clear canvas with specific color
+    love.graphics.clear(0/255, 45/255, 52/255, 255/255)
+    love.graphics.printf('Hello Pong!', 0, 20, VIRTUAL_WIDTH, 'center')
+
+    -- Render the rectangles on the sides
+    love.graphics.rectangle('fill', 10, 30, 5, 20)
+    love.graphics.rectangle('fill', VIRTUAL_WIDTH - 10, VIRTUAL_HEIGHT - 50, 5, 20)
+    love.graphics.rectangle('fill', VIRTUAL_WIDTH / 2 - 2, VIRTUAL_HEIGHT / 2 - 2, 4, 4)
+
     push:apply('end')
 end
